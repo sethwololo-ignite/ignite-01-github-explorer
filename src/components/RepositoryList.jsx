db700151e5316e6b8892927f8ba9b56b7115335d
Matheus Henrique <54484095+sethwololo@users.noreply.github.com>
2021-03-09 11:02:08 -0300

    Primeiro componente React

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,44 @@
+export default function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Repository List</h1>
+
+      <ul>
+        <li>
+          <strong>repoName</strong>
+          <p>Forms in React</p>
+
+          <a href="#">
+            Acess repository
+          </a>
+        </li>
+
+        <li>
+          <strong>repoName</strong>
+          <p>Forms in React</p>
+
+          <a href="#">
+            Acess repository
+          </a>
+        </li>
+
+        <li>
+          <strong>repoName</strong>
+          <p>Forms in React</p>
+
+          <a href="#">
+            Acess repository
+          </a>
+        </li>
+        <li>
+          <strong>repoName</strong>
+          <p>Forms in React</p>
+
+          <a href="#">
+            Acess repository
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
