305f199142b9fdc181a10429c270ee9538e6cc61
Matheus Henrique <54484095+sethwololo@users.noreply.github.com>
2021-03-09 13:07:09 -0300

    Estilização da listagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import RepositoryList from './components/RepositoryList'
-import Counter from './components/Counter'
 import './styles/global.scss'
 
 export default function App() {
   return (
     <>
       <RepositoryList />
-      <Counter />
     </>
   )
 }
