045f5648c4a2668ddd1c506b861305fa5bc963fc
Matheus Henrique <54484095+sethwololo@users.noreply.github.com>
2021-03-09 14:51:27 -0300

    Aulas finalizadas

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -14,14 +14,14 @@ export default function RepositoryList() {
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
-    fetch('https://api.github.com/orgs/rocketseat/repos')
+    fetch('https://api.github.com/users/sethwololo/repos')
       .then(response => response.json())
       .then(repositories => setRepositories(repositories));
   }, []);
 
   return (
     <section className="repository-list">
-      <h1>Repository List</h1>
+      <h1>Sethwololo's repositories</h1>
 
       <ul>
         {repositories.length <= 0 ? <h1>Carregando...</h1> : repositories.map(repository => <RepositoryItem repository={repository} key={repository.id} />)}
