ca557f02276d3443f720242e9c0c82ae712355e8
Matheus Henrique <54484095+sethwololo@users.noreply.github.com>
2021-03-09 13:30:43 -0300

    Utilizando o useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,29 @@
 import RepositoryItem from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+import { useEffect, useState } from "react";
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
+// const repository = {
+//   name: 'unform',
+//   description: 'Forms in React',
+//   link: 'https://github.com/unform/unform'
+// }
 
 export default function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(repositories => setRepositories(repositories));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Repository List</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.length <= 0 ? <h1>Carregando...</h1> : repositories.map(repository => <RepositoryItem repository={repository} />)}
       </ul>
     </section>
   );
