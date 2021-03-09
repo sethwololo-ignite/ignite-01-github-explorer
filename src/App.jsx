900fb88e8ebb5fe5dfcaf4d33fb80c3b78ea7ed1
Matheus Henrique <54484095+sethwololo@users.noreply.github.com>
2021-03-09 11:46:14 -0300

    Estado do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import RepositoryList from './components/RepositoryList'
+import Counter from './components/Counter'
 import './styles/global.scss'
 
 export default function App() {
   return (
     <>
       <RepositoryList />
+      <Counter />
     </>
   )
 }
